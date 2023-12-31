fb081e4b068462bbc1dd532bbe6ad69a1d6e08cf
asibkhanpro <asibkhanpro@gmail.com>
2023-12-31 11:28:21 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+    // Hamburger-menu
+    $('.hamburger-menu').on('click', function () {
+      $('.hamburger-menu .line-top, .responsive-menu').toggleClass('current');
+      $('.hamburger-menu .line-center').toggleClass('current');
+      $('.hamburger-menu .line-bottom').toggleClass('current');
+  });
